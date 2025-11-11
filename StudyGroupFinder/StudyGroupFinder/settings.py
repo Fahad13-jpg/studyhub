747f3794baf7484c10212e23dd32e4df034ad568
Muhamad Fahad <fah7861234@gmail.com>
2025-11-11 05:48:51 -0800

    Add requirements.txt and ready for deployment

--- StudyGroupFinder/StudyGroupFinder/settings.py
+++ StudyGroupFinder/StudyGroupFinder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*d15)-0lg=#j-01_i+o6qhva3x8s#r$mypx)jssv8$8sis=-@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
